8b14873177274442a403eae0b16309e888193b0c
waseemprogrammer <waseemprogrammerofficial@gmail.com>
2024-10-17 22:09:57 +0500

    lecture -01 php intro , comments , and PHP syntax & Variables

---
+++ lecture-01/README.md
@@ -0,0 +1,6 @@
+# Lecture -01 
+
+PHP Introduction and Basics of PHP.
+
+In this Chapter we have learned about PHP Introduction
+and PHP Variables
---
+++ lecture-01/index.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Title</title>
+</head>
+<body>
+    <?php
+    //    PHP Variables
+//    $username = "ghazi";
+//    $phoneNumber =  20.2;
+
+//    Output Variables
+//    print($phoneNumber);
+//     echo $phoneNumber;
+//    String
+//Integer
+//Float (floating point numbers - also called double)
+//Boolean - True / False
+//Array ()
+    ////Object
+//NULL
+//Resource
+//Get the Type
+//    var_dump($phoneNumber);
+//    PHP Variables Scope
+//    Global and Local Scope
+//    PHP The global Keyword
+
+//    function greeting(){
+//        global $username;
+//        $username = "Ghazi";
+//        echo $username;
+//    }
+//
+//    greeting();
+
+//    PHP The static Keyword
+
+//    function greeting()
+//    {
+//        static $username = "Ghazi";
+//        echo $username;
+//    }
+//    greeting();
+    $name = 'Linus';
+    echo "<h1>Hello $name</h1>";
+    echo "<h1>Hello $name</h1>";
+    ?>
+</body>
+</html>
